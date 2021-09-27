95368ae40ca3b4b75570fc5872d327a108140514
muhakesss <91071295+muhakesss@users.noreply.github.com>
2021-09-27 17:00:29 +0300

    Create smile.py

---
+++ Lab3/smile.py
@@ -0,0 +1,21 @@
+from graph import*
+
+penColor("black")
+penSize(1)
+brushColor("yellow")
+circle(200,150,100)
+brushColor('black')
+rectangle(140,180, 260,200)
+brushColor("red")
+circle(170, 130, 20)
+circle(230, 130, 10)
+brushColor("black")
+circle(170, 130, 10)
+circle(230, 130, 5)
+penColor("black")
+penSize(8)
+line(110, 80, 200, 120)
+line(300, 90, 210, 125)
+
+
+run()
